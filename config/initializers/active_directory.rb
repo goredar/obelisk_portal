57e1c7421c16f226b4a9ec4c99094fd3166b87db
goredar <goredar@gmail.com>
2015-02-08 16:20:48 +0300

    up

--- config/initializers/active_directory.rb
+++ config/initializers/active_directory.rb
@@ -2,17 +2,29 @@ conf = YAML.load_file(Rails.root.join('config/ad.yml')).fetch(Rails.env)
 
 module ActiveDirectory
   module Contacts
+
     @mapping = {}
+    @mutex = Mutex.new
+
     def self.setup(mapping)
       @mapping = mapping
     end
     def self.update
-      users = ActiveDirectory::User.find(:all)
-      Contact.delete_all
-      users.reject { |u| u[:name].include? "Test User" }.map do |u|
-        k = @mapping.values
-        v = @mapping.keys.map { |attr| u.valid_attribute?(attr) ? u.send(attr).force_encoding("UTF-8") : nil }
-        Contact.new(Hash[k.zip v]).save!
+      @mutex.synchronize do
+        start_time = DateTime.now.utc
+        users = ActiveDirectory::User.find(:all)
+        users.reject! { |u| u[:name].include? "Test User" }.map! do |u|
+          k = @mapping.values
+          v = @mapping.keys.map { |attr| u.valid_attribute?(attr) ? u.send(attr).force_encoding("UTF-8") : nil }
+          Hash[k.zip v]
+        end
+        # Update or create
+        users.each do |user|
+          user_in_db = Contact.find_by_login user.fetch(:login)
+          user_in_db ? (user_in_db.update!(user); user_in_db.touch) : Contact.new(user).save!
+        end
+        # Delete old records (not in AD now)
+        Contact.where("updated_at < :start_time", start_time: start_time).find_each { |c| c.destroy }
       end
     end
   end

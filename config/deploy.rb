60a2909186c322f8a3b360c10a432cd16d305f6a
Vitaliy Sazonenko <goredar@gmail.com>
2016-06-16 14:04:27 +0300

    Merge pull request #1 from Substance0510/patch-1
    Update ru.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -43,6 +43,7 @@ namespace :deploy do
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
+      execute "sudo service nginx restart"
       # Here we can do anything such as:
       # within release_path do
       #   execute :rake, 'cache:clear'
